d0fb837d3c4ebbb6c98ea4ecce7422f1c0b391ab
wjw465150 <wjw465150@gmail.com>
2013-08-19 13:44:20 +0800

    @wjw_add: JMX添加status()方法来获取LevelDB数据库的当前状态.

--- src/org/sqs4j/jmx/Sqs4J.java
+++ src/org/sqs4j/jmx/Sqs4J.java
@@ -2,17 +2,22 @@ package org.sqs4j.jmx;
 
 import org.sqs4j.Sqs4jApp;
 
-public class Sqs4J implements Sqs4JMBean{
+public class Sqs4J implements Sqs4JMBean {
   private Sqs4jApp _app;
 
   public Sqs4J(Sqs4jApp app) {
     _app = app;
   }
-  
+
   @Override
   public boolean flush() {
     _app.flush();
     return true;
+  }
+
+  @Override
+  public String status() {
+    return _app._db.getProperty("leveldb.stats");
   }
 
 }
--- src/org/sqs4j/jmx/Sqs4JMBean.java
+++ src/org/sqs4j/jmx/Sqs4JMBean.java
@@ -2,5 +2,5 @@ package org.sqs4j.jmx;
 
 public interface Sqs4JMBean {
   boolean flush();
-  
+  String status();
 }
